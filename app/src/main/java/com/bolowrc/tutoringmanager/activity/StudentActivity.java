b80b0210bc88df192e03dd9d4b8b114ee38430af
Davide Bolognini <davide.bolognini@gmail.com>
2015-02-15 12:46:17 +0100

    Refactoring newer student and student list

--- app/src/main/java/com/bolowrc/tutoringmanager/activity/StudentActivity.java
+++ app/src/main/java/com/bolowrc/tutoringmanager/activity/StudentActivity.java
@@ -1,93 +1,46 @@
 package com.bolowrc.tutoringmanager.activity;
 
-import android.content.Context;
-import android.database.Cursor;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CursorAdapter;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import com.bolowrc.tutoringmanager.R;
 import com.bolowrc.tutoringmanager.repository.RepositoryException;
 import com.bolowrc.tutoringmanager.repository.StudentRepository;
 
-import static com.bolowrc.tutoringmanager.repository.DatabaseStrings.ID;
-import static com.bolowrc.tutoringmanager.repository.DatabaseStrings.STUDENT_FIRSTNAME;
-import static com.bolowrc.tutoringmanager.repository.DatabaseStrings.STUDENT_LASTNAME;
-import static com.bolowrc.tutoringmanager.repository.DatabaseStrings.STUDENT_SCHOOL;
+import static android.widget.Toast.LENGTH_LONG;
+import static android.widget.Toast.makeText;
 
 public class StudentActivity extends ActionBarActivity {
 
-    private StudentRepository db = null;
-    private CursorAdapter adapter;
-    private ListView listview = null;
-    private View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            int position = listview.getPositionForView(v);
-            long id = adapter.getItemId(position);
-            if (db.delete(id))
-                adapter.changeCursor(db.getStudents());
-        }
-    };
+    private StudentRepository studentRepository = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
-        db = new StudentRepository(this);
-        listview = (ListView) findViewById(R.id.listView);
-        Cursor crs = db.getStudents();
-        adapter = new CursorAdapter(this, crs, 0) {
-            @Override
-            public View newView(Context ctx, Cursor arg1, ViewGroup arg2) {
-                View v = getLayoutInflater().inflate(R.layout.listactivity_row, null);
-                return v;
-            }
-
-            @Override
-            public void bindView(View v, Context arg1, Cursor crs) {
-                String firstName = crs.getString(crs.getColumnIndex(STUDENT_FIRSTNAME));
-                String lastName = crs.getString(crs.getColumnIndex(STUDENT_LASTNAME));
-                String school = crs.getString(crs.getColumnIndex(STUDENT_SCHOOL));
-                TextView txt = (TextView) v.findViewById(R.id.txt_firstName);
-                txt.setText(firstName);
-                txt = (TextView) v.findViewById(R.id.txt_lastName);
-                txt.setText(lastName);
-                txt = (TextView) v.findViewById(R.id.txt_school);
-                txt.setText(school);
-                ImageButton imgbtn = (ImageButton) v.findViewById(R.id.btn_delete);
-                imgbtn.setOnClickListener(clickListener);
-            }
-
-            @Override
-            public long getItemId(int position) {
-                Cursor crs = adapter.getCursor();
-                crs.moveToPosition(position);
-                return crs.getLong(crs.getColumnIndex(ID));
-            }
-        };
-
-        listview.setAdapter(adapter);
+        studentRepository = new StudentRepository(this);
     }
 
-    public void salva(View v) {
-        EditText firstName = (EditText) findViewById(R.id.firstName);
-        EditText lastName = (EditText) findViewById(R.id.lastName);
-        EditText school = (EditText) findViewById(R.id.school);
-        if (firstName.length() > 0 && lastName.length() > 0) {
+    public void saveStudent(View v) {
+        if (!getStringFrom(R.id.firstNameInput).isEmpty() && !getStringFrom(R.id.schoolInput).isEmpty()) {
             try {
-                db.save(firstName.getEditableText().toString(), lastName.getEditableText().toString(), school.getEditableText().toString());
+                studentRepository.save(getStringFrom(R.id.firstNameInput), getStringFrom(R.id.lastNameInput), getStringFrom(R.id.schoolInput));
+                makeText(getApplicationContext(), getString(R.string.studentSaved), LENGTH_LONG).show();
+                Intent i = new Intent(this, StudentsActivity.class);
+                startActivity(i);
             } catch (RepositoryException e) {
-                e.printStackTrace();
+                makeText(getApplicationContext(), getString(R.string.problemsSavingStudent), LENGTH_LONG).show();
             }
-            adapter.changeCursor(db.getStudents());
+        } else {
+            makeText(getApplicationContext(), getString(R.string.validationSavingStudent), LENGTH_LONG).show();
         }
+    }
+
+    private String getStringFrom(int firstNameTextId) {
+        return ((EditText) findViewById(firstNameTextId)).getEditableText().toString();
     }
 
 }
--- app/src/main/java/com/bolowrc/tutoringmanager/activity/StudentsActivity.java
+++ app/src/main/java/com/bolowrc/tutoringmanager/activity/StudentsActivity.java
@@ -1,47 +1,107 @@
 package com.bolowrc.tutoringmanager.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.bolowrc.tutoringmanager.R;
+import com.bolowrc.tutoringmanager.repository.StudentRepository;
+
+import static com.bolowrc.tutoringmanager.repository.DatabaseStrings.ID;
+import static com.bolowrc.tutoringmanager.repository.DatabaseStrings.STUDENT_FIRSTNAME;
+import static com.bolowrc.tutoringmanager.repository.DatabaseStrings.STUDENT_LASTNAME;
+import static com.bolowrc.tutoringmanager.repository.DatabaseStrings.STUDENT_SCHOOL;
 
 public class StudentsActivity extends ActionBarActivity {
+
+
+    private CursorAdapter adapter;
+    private ListView listview = null;
+    private StudentRepository studentRepository = null;
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int position = listview.getPositionForView(v);
+            long id = adapter.getItemId(position);
+            if (studentRepository.delete(id))
+                adapter.changeCursor(studentRepository.getStudents());
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_students);
+
+        studentRepository = new StudentRepository(this);
+
+
+        listview = (ListView) findViewById(R.id.listView);
+        Cursor crs = studentRepository.getStudents();
+        adapter = new CursorAdapter(this, crs, 0) {
+            @Override
+            public View newView(Context ctx, Cursor arg1, ViewGroup arg2) {
+                View v = getLayoutInflater().inflate(R.layout.listactivity_row, null);
+                return v;
+            }
+
+            @Override
+            public void bindView(View v, Context arg1, Cursor crs) {
+                String firstName = crs.getString(crs.getColumnIndex(STUDENT_FIRSTNAME));
+                String lastName = crs.getString(crs.getColumnIndex(STUDENT_LASTNAME));
+                String school = crs.getString(crs.getColumnIndex(STUDENT_SCHOOL));
+                TextView txt = (TextView) v.findViewById(R.id.txt_firstName);
+                txt.setText(firstName);
+                txt = (TextView) v.findViewById(R.id.txt_lastName);
+                txt.setText(lastName);
+                txt = (TextView) v.findViewById(R.id.txt_school);
+                txt.setText(school);
+                ImageButton imgbtn = (ImageButton) v.findViewById(R.id.btn_delete);
+                imgbtn.setOnClickListener(clickListener);
+            }
+
+            @Override
+            public long getItemId(int position) {
+                Cursor crs = adapter.getCursor();
+                crs.moveToPosition(position);
+                return crs.getLong(crs.getColumnIndex(ID));
+            }
+        };
+
+        listview.setAdapter(adapter);
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_students, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
 
-    public void newStudent(View view) {
+    public void newerStudentActivity(View view) {
         Intent i = new Intent(this, StudentActivity.class);
         startActivity(i);
     }
