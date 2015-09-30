df2b35599bca347bfb5a232cd91bcb5978865d3b
Davide Bolognini <davide.bolognini@rumbo.com>
2015-09-30 23:05:01 +0200

    Revert test

--- app/src/main/java/com/bolowrc/tutoringmanager/activity/StudentsActivity.java
+++ app/src/main/java/com/bolowrc/tutoringmanager/activity/StudentsActivity.java
@@ -122,5 +122,6 @@ public class StudentsActivity extends ActionBarActivity {
             if (studentRepository.delete(id))
                 adapter.changeCursor(studentRepository.getStudentsData());
         }
+    };
 
 }
